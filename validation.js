9c7bfdadb81a153b90c61b5681ce00c0f02a208d
Dan Fletcher <dannyjfletcher@outlook.com>
2016-03-21 16:52:39 +0000

    accept unclosed <td> tags
    A regex has been added to detect valid HTML with unclosed <td> tags.
    The addedCellsP pattern has been simplified to including only a single <td> tag,
    so that now we can get a passing pattern even when <td> tags are left unclosed.
    
    The <td> count is then verified when we do our regex check for valid lines with
    unclosed <td>'s.
    
    With this change the issue of valid HTML not passing the grader is being addressed.
    Now, <td> tags do not need to be closed in order to pass the challenge.

--- validation.js
+++ validation.js
@@ -1,3 +1,13 @@
+/*******************
+ * Regex Checks
+ * *****************/
+var regex = [
+        {
+            // used for checking unclosed <td> tags - this should pass.
+            re: /(?=<td>).*(?=<td>).*(?=<td>)/
+        }
+    ];
+
 staticTest($._("Add rows of meals"), function() {
     var result = null;
     var descrip = $._("This is a webpage to show the dinner menu for a restaurant. We've created the initial table with three columns for the meal, ingredients, and price. Now, add three rows of data to the table, one row for each meal.");
@@ -5,7 +15,12 @@ staticTest($._("Add rows of meals"), function() {
     var displayP = "<tbody>\n  <_>\n    <_>...</_>\n    <_>...</_>\n    <_>...</_>\n  </_>\n</tbody>";
     
     var addedRowsP = {'tbody tr:not(:empty)': 3};
-    var addedCellsP = {'tbody tr td:not(:empty)': 9};
+    
+    // we only check for one <td> to be pressent so that we can give the grader a passing
+    // object. 
+    // the count is checked along with the regex so we can pass unclosed <td> tags.
+    // - Dan Fletcher (https://github.com/DanJFletcher)
+    var addedCellsP = 'tbody tr td:not(:empty)';
     
     result = allPass(htmlMatch(addedRowsP), htmlMatch(addedCellsP));
     
@@ -28,6 +43,12 @@ staticTest($._("Add rows of meals"), function() {
             result = fail($._("You're using <th> cells inside your <tbody> rows, which isn't correct. Do you remember the other tag that you're meant to use for the cells inside the <tbody>? Hint: you're storing 'tabular data' in them."));
         } else if (htmlMatches(addedOneRowP)) {
             result = fail($._("Nice! Make sure you add at least three rows."));
+        }
+    } else if (passes(result)) {
+        if (regex[0].re.test($doc.context.body.innerHTML) && htmlMatches({"tr td": 9})) {
+            result = pass(); 
+        } else {
+            result = fail();
         }
     }
     if (htmlMatches(multipleTBodysP)) {
